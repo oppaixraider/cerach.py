b53b298f77833029d42a9eda3b651baa2094e89e
oppaixraider <84872618+oppaixraider@users.noreply.github.com>
2021-06-11 10:49:58 +0700

    Add files via upload

---
+++ cerach.py
@@ -0,0 +1,50 @@
+#go to nico-nico.chatango.com mother fucker
+
+
+
+import websocket
+import random
+import re
+import time
+import urllib.request
+import urllib.parse
+import memek
+import peju
+import tubu
+import agung
+import adit
+
+
+try:
+    file = open("RankSystem/rank3.txt", "r")
+    for name in file.readlines():
+        if len(name.strip()) > 0:
+            rank3.append(name.strip())
+    print("[INFO]rank3 loaded...")
+    file.close()
+except:
+    print("[ERROR]no file named rank3")
+    print("2 second to read the error")
+    time.sleep(2)
+    "DELETED" : 1 << 0 ,
+    "EDIT_MODS" :  1 << 1 ,
+    "EDIT_MOD_VISIBILITY" : 1 << 2 ,
+    "EDIT_BW" : 1 << 3 ,
+    "EDIT_RESTRICTIONS" : 1 << 4 ,
+    "EDIT_GROUP" : 1 << 5 ,
+    "SEE_COUNTER" : 1 << 6 ,
+    "SEE_MOD_CHANNEL" : 1 << 7 ,
+    "SEE_MOD_ACTIONS" : 1 << 8 ,
+    "EDIT_NLP" : 1 << 9 ,
+    "EDIT_GP_ANNC" : 1 << 10 ,
+    "EDIT_ADMINS" : 1 << 11 ,
+    "EDIT_SUPERMODS" : 1 << 12 ,
+    "NO_SENDING_LIMITATIONS" : 1 << 13 ,
+    "SEE_IPS" : 1 << 14 ,
+    "CLOSE_GROUP" : 1 << 15 ,
+    "CAN_BROADCAST" : 1 << 16 ,
+    "MOD_ICON_VISIBLE" : 1 << 17 ,
+    "IS_STAFF" : 1 << 18 ,
+    "STAFF_ICON_VISIBLE" : 1 << 19 ,
+    "UNBAN_ALL" : 1 << 20
+    }
